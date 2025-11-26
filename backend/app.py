d51cb8a38c2197214bf573dff1e13b12b330fe7d
Sohail Shaikh <sohailbhaishaikh7860@gmail.com>
2025-11-26 19:59:18 +0530

    Initial commit

--- backend/app.py
+++ backend/app.py
@@ -604,3 +604,16 @@ if __name__ == '__main__':
     with app.app_context():
         db.create_all()
     app.run(debug=True)
+
+from flask import Flask, send_from_directory
+import os
+
+app = Flask(__name__, static_folder="static")
+
+@app.route('/', defaults={'path': ''})
+@app.route('/<path:path>')
+def serve(path):
+    if path != "" and os.path.exists("static/" + path):
+        return send_from_directory("static", path)
+    else:
+        return send_from_directory("static", "index.html")
